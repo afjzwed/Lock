cb412540db99b03defc6dd1f956dac1b86df28a2
afjzwed <afjzwed@163.com>
2018-09-03 15:06:18 +0800

    王煌 测试提交 2018.9.3

---
+++ app/src/main/java/com/cxwl/menjin/lock/MainApplication.java
@@ -0,0 +1,60 @@
+package com.cxwl.menjin.lock;
+
+import android.app.Application;
+import android.app.PendingIntent;
+
+import com.cxwl.menjin.lock.face.ArcsoftManager;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.log.LoggerInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by William on 2018/8/22.
+ */
+
+public class MainApplication  extends Application {
+
+    private static MainApplication application;
+    PendingIntent restartIntent;
+
+    @Override
+    public void onCreate() {
+
+        application = this;
+        //人脸识别的数据存本地
+        ArcsoftManager.getInstance().initArcsoft(this);//虹软人脸识别初始化
+
+        super.onCreate();
+
+        //okhttp初始化
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(new LoggerInterceptor("okhttp", true))
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS).readTimeout(10000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
+//        Intent intent = new Intent();
+//        // 参数1：包名，参数2：程序入口的activity
+//        intent.setClassName("com.cxwl.hurry.doorlock", "com.cxwl.hurry.doorlock.ui.activity.MainActivity");
+//        restartIntent = PendingIntent.getActivity(getApplicationContext(), 0,
+//                intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+//        Thread.setDefaultUncaughtExceptionHandler(restartHandler); // 程序崩溃时触发线程
+    }
+
+//    public Thread.UncaughtExceptionHandler restartHandler = new Thread.UncaughtExceptionHandler() {
+//        @Override
+//        public void uncaughtException(Thread thread, Throwable ex) {
+//            DLLog.e("崩溃重启", "错误 " + ex);
+//            AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//            mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000,
+//                    restartIntent); // 1秒钟后重启应用
+//            android.os.Process.killProcess(android.os.Process.myPid()); // 自定义方法，关闭当前打开的所有avtivity
+//        }
+//    };
+
+    public static MainApplication getApplication() {
+        return application;
+    }
+}
